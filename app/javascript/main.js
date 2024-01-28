bced651f36a59149213c01084467be4f27723f7d
Tomas Radic <tomas.radic@gmail.com>
2024-01-28 20:21:45 +0100

    Removed unused JS code.

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -29,21 +29,21 @@
 
 
     // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
+    // $(document).ready(function () {
+    //     var $videoSrc;
+    //     $('.btn-play').click(function () {
+    //         $videoSrc = $(this).data("src");
+    //     });
+    //     console.log($videoSrc);
+    //
+    //     $('#videoModal').on('shown.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    //     })
+    //
+    //     $('#videoModal').on('hide.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc);
+    //     })
+    // });
 
 
     // Scroll to Bottom
@@ -84,34 +84,34 @@
 
 
     // Gallery carousel
-    $(".gallery-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            },
-            1200:{
-                items:5
-            }
-        }
-    });
+    // $(".gallery-carousel").owlCarousel({
+    //     autoplay: false,
+    //     smartSpeed: 1500,
+    //     dots: false,
+    //     loop: true,
+    //     nav : true,
+    //     navText : [
+    //         '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+    //         '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    //     ],
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         576:{
+    //             items:2
+    //         },
+    //         768:{
+    //             items:3
+    //         },
+    //         992:{
+    //             items:4
+    //         },
+    //         1200:{
+    //             items:5
+    //         }
+    //     }
+    // });
     
 })(jQuery);
 
