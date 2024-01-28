275aeffa842a19bd67e1b45d50735f9a836633bf
Tomas Radic <tomas.radic@gmail.com>
2024-01-28 20:56:41 +0100

    Pins for JS.

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,10 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "jquery.min.js", preload: true
 pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin "jquery-ui", to: "jquery-ui.min.js", preload: true
+
+# pin "jquery_ui"
+pin "easing.min"
+pin "waypoints.min"
+pin "owl.carousel"
+pin "isotope.pkgd.min"
+pin "main"
